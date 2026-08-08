6d5ff5561ecea41d3ae0cbfeced1c9f392dfb2b5
Muaaz Kasbati <93757841+muaazkasbati@users.noreply.github.com>
2026-08-08 12:17:10 +0500

    Update ddd

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,25 @@
 // pages/_app.js
 import "@/styles/globals.css";
 import useLenis from "@/hooks/useLenis";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   useLenis();
   return (
-    <Component {...pageProps} />
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-0E7Q7L7JFP"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-0E7Q7L7JFP');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
   );
 }
